98d3086cd6d2668e526d960037c638fe54473b5c
tranghuynh1204 <145643390+tranghuynh1204@users.noreply.github.com>
2024-03-22 11:40:57 +0700

    initial

---
+++ src/main/java/hcmute/web_cosmetic/WebCosmeticApplication.java
@@ -0,0 +1,13 @@
+package hcmute.web_cosmetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCosmeticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCosmeticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hcmute/web_cosmetic/WebCosmeticApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmute.web_cosmetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCosmeticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
