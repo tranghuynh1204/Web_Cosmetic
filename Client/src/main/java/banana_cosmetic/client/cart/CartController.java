11f9ad9907d68b557c10a1f4b4acee320ed4487e
tranghuynh1204 <145643390+tranghuynh1204@users.noreply.github.com>
2024-05-10 10:31:53 +0700

    test máy con chúi

--- Client/src/main/java/banana_cosmetic/client/cart/CartController.java
+++ Client/src/main/java/banana_cosmetic/client/cart/CartController.java
@@ -26,6 +26,7 @@ public class CartController {
         }
 
     }
+    //thật sưj cay nha
     @DeleteMapping("/remove")
     public ResponseEntity<String> removeFromCart(@RequestBody List<Long> lineItemIds){
         try{
