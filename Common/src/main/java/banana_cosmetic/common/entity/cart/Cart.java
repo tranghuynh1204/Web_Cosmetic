4e74280f316f8fa3bdc4850cd816ab40295d6474
tranghuynh1204 <145643390+tranghuynh1204@users.noreply.github.com>
2024-05-10 09:25:42 +0700

    api cart

--- Common/src/main/java/banana_cosmetic/common/entity/cart/Cart.java
+++ Common/src/main/java/banana_cosmetic/common/entity/cart/Cart.java
@@ -16,15 +16,39 @@ public class Cart extends IdBasedEntity {
     @OneToOne
     private User customer;
     @JoinColumn(name = "cart_id")
-    @OneToMany(cascade = CascadeType.ALL,orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LineItem> items;
 
-    public Cart() {}
+    public Cart() {
+    }
 
     public Cart(User customer) {
         this.customer = customer;
         items = new ArrayList<>();
     }
 
+    public void addLineItem(LineItem lineitem) {
+
+        for (LineItem existingLineItem : items) {
+            if (existingLineItem.getProduct().getId().equals(lineitem.getProduct().getId())) {
+                existingLineItem.setQuantity(existingLineItem.getQuantity() + lineitem.getQuantity());
+                return;
+            }
+        }
+        items.add(lineitem);
+    }
+
+    public void removeLineItemsByIds(List<Long> lineItemIds) {
+        this.getItems().removeIf(item -> lineItemIds.contains(item.getId()));
+    }
+
+    public void updateLineItemQuantity(Long lineItemId, int quantity) {
+        for (LineItem item : items) {
+            if (item.getId().equals(lineItemId)) {
+                item.setQuantity(quantity);
+                break;
+            }
+        }
+    }
 
 }
