2f896150128283aa54551fa54ac268fe5833ea38
tranghuynh1204 <145643390+tranghuynh1204@users.noreply.github.com>
2024-05-10 10:24:40 +0700

    api cart

--- Client/src/main/java/banana_cosmetic/client/cart/CartController.java
+++ Client/src/main/java/banana_cosmetic/client/cart/CartController.java
@@ -30,7 +30,7 @@ public class CartController {
     public ResponseEntity<String> removeFromCart(@RequestBody List<Long> lineItemIds){
         try{
             service.deleteFromCart(lineItemIds);
-            return new ResponseEntity<>("Xoá thành công", HttpStatus.OK); // Trả về brand mới với mã trạng thái 200 OK
+            return new ResponseEntity<>("Xoá thành công", HttpStatus.OK); // Trả về brand mới với mã trạng thái 200 OK.
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST); // Trả về thông báo lỗi với mã trạng thái 400 Bad Request
         }
